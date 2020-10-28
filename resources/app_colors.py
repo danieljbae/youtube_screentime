f8b6261fcb922396da9641d553ff0bd24db83887
Daniel Bae <danieljbaee@hotmail.com>
2020-10-27 22:25:57 -0700

    updated file structure

---
+++ resources/app_colors.py
@@ -0,0 +1,52 @@
+category_color_map = {
+# ---- Music (Orange)
+"Music" : "rgb(252,141,89)",
+
+# Fictional Stories (Redish)
+"Film & Animation" : "rgb(254, 224,210)",
+"Gaming": "rgb(214, 96,77)",
+"Movies": "rgb(251, 106,74)",
+"Anime/Animation": "rgb(254, 224,210)",
+"Short Movies": "rgb(252, 146,114)",
+"Drama": "rgb(252, 146,114)",
+"Thriller": "rgb(252, 146,114)",
+"Shorts": "rgb(252, 146,114)",
+"Shows": "rgb(252, 146,114)",
+"Trailers": "rgb(252, 146,114)",
+"Drama": "rgb(252, 146,114)",
+"Horror": "rgb(252, 146,114)",
+"Action/Adventure": "rgb(252, 146,114)",
+"Classics": "rgb(252, 146,114)",
+
+# Real life Events (Bluish)
+"Sports": "rgb(222, 235,247)",
+"Travel & Events": "rgb(198, 219,239)",
+"News & Politics": "rgb(158, 202,225)",
+"Nonprofits & Activism": "rgb(107, 174,214)",
+"Foreign": "rgb(252, 146,114)",
+"Family": "rgb(252, 146,114)",
+"Autos & Vehicles": "rgb(252, 146,114)",
+"Pets & Animals": "rgb(252, 146,114)",
+
+# Real life People (Purplush)
+"Videoblogging": "rgb(128, 125, 186)",
+"People & Blogs": "rgb(158, 154, 200)",
+"Comedy" : "rgb(84, 39, 143)",
+"Documentary" : "rgb(188, 189, 220)",
+"Entertainment": "rgb(254, 247,118)",
+
+# Learning (Greeinish)
+"Howto & Style": "rgb(85, 174, 91)",
+"Education": "rgb(111, 201, 163)",
+"Science & Technology": "rgb(186, 228, 174)",
+"Sci-Fi/Fantasy": "rgb(156, 209, 143)"
+}
+
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF',
+    "tab_border": "dimgrey",
+    "tab_primary": "dimgrey",
+    "tab_background": "darkgrey"
+    }
---
+++ resources/categorieshashmap.py
@@ -0,0 +1,36 @@
+#### Youtube Video Categories - Creates hashmap of all youtube's current category ID's 
+# Source: https://www.geeksforgeeks.org/youtube-data-api-for-handling-videos-set-3/
+
+
+from googleapiclient.discovery import build
+
+# Restrict Key on Google API # Look up how to make env variable for secret key 
+api_key = 'AIzaSyDbBHaLv3JAFa5d4zVQFz_uzqKszztI8kw' 
+
+# Service is Youtube API 
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+def youtube_video_categories(): 
+      
+    # calling the videoCategory.list method 
+    # to retrieve youtube video categories result 
+    video_category = youtube.videoCategories( 
+       ).list(part ='snippet', regionCode ='US').execute()  # specific to region
+      
+    # extracting the results 
+    # from video_category response 
+    results = video_category.get("items", []) 
+  
+    # empty list to store video category metadata 
+    videos_categories = {}
+      
+    # extracting required info 
+    # from each result object 
+    for result in results: 
+
+        # key = categoryID || val = (Category string, assignable)
+        videos_categories[result["id"]] = result["snippet"]["title"],result["snippet"]["assignable"]
+
+    return videos_categories
+      
